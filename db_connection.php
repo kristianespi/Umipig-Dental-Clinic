4a4a95d52f2634d1d76274b4954581212a575855
kristianespi <kristianespinase01@gmail.com>
2025-06-04 20:54:10 +0800

    first

--- db_connection.php
+++ db_connection.php
@@ -1,13 +1,18 @@
 <?php
-$host = 'localhost';      
-$dbname = 'clinic_db'; 
-$username = 'root';       
-$password = '';         
+require_once __DIR__ . '/vendor/autoload.php';
 
-$conn = new mysqli($host, $username, $password, $dbname);
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+$conn = new mysqli($_ENV['DB_HOST'], $_ENV['DB_USER'], $_ENV['DB_PASS'], $_ENV['DB_NAME']);
+
 
 // Check for connection error
 if ($conn->connect_error) {
     die('Connection failed: ' . $conn->connect_error);
 }
+
+
+
+
 ?>
